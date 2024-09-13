16463b507c10589ced1352adcaa24e8b634bdebf
Alex Moyer <alexmoyer@Alexs-MacBook-Air.local>
2024-09-13 08:57:17 -0400

    first commit

---
+++ src/test/java/com/alexcasey/nasa_apod/NasaApodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexcasey.nasa_apod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NasaApodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
